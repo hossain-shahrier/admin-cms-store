66c0cbd5a268d9ca70a88f2376834445d52d3cb1
Shahrier Hossain <hossainshahrier.shm@gmail.com>
2023-10-06 16:31:32 +0600

    Clerk Authentication

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,10 @@
+import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 
 export default function Home() {
-  return <p>Hello CMS</p>;
+  return (
+    <Button size="default" variant="ghost">
+      Click me
+    </Button>
+  );
 }
