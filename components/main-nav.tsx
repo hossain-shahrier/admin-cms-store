4453d901a0c6ced3be139b9a7cf7be4cb00a6aab
Shahrier Hossain <hossainshahrier.shm@gmail.com>
2023-10-08 01:30:49 +0600

    Products created

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -17,6 +17,11 @@ export function MainNav({
       active: pathname === `/${params.storeId}`,
     },
     {
+      href: `/${params.storeId}/products`,
+      label: 'Products',
+      active: pathname === `/${params.storeId}/products`,
+    },
+    {
       href: `/${params.storeId}/billboards`,
       label: 'Billboards',
       active: pathname === `/${params.storeId}/billboards`,
--- components/ui/image-upload.tsx
+++ components/ui/image-upload.tsx
@@ -65,7 +65,7 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
               onClick={onClick}
             >
               <ImagePlus className="h-4 w-4 mr-2" />
-              Upload an Image
+              Upload Image
             </Button>
           );
         }}
